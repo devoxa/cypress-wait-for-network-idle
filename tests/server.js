d0729a6757c05ba9790866b1545621ec00936263
queicherius <queicherius@gmail.com>
2021-12-23 15:52:23 +0000

    More test improvements

--- tests/server.js
+++ tests/server.js
@@ -1,16 +1,41 @@
 const fs = require('fs')
 const express = require('express')
 
-const PAGE_HTML = fs.readFileSync(__dirname + '/page.html', 'utf-8')
-
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send(PAGE_HTML)
+app.get('/no-requests', (request, response) => {
+  const html = fs.readFileSync(__dirname + '/no-requests.html', 'utf-8')
+  response.send(html)
 })
 
-app.get('/data', (request, response) => {
+app.get('/fast-requests', (request, response) => {
+  const html = fs.readFileSync(__dirname + '/fast-requests.html', 'utf-8')
+  response.send(html)
+})
+
+app.get('/delayed-fast-requests', (request, response) => {
+  const html = fs.readFileSync(__dirname + '/delayed-fast-requests.html', 'utf-8')
+  response.send(html)
+})
+
+app.get('/slow-requests', (request, response) => {
+  const html = fs.readFileSync(__dirname + '/slow-requests.html', 'utf-8')
+  response.send(html)
+})
+
+app.get('/delayed-slow-requests', (request, response) => {
+  const html = fs.readFileSync(__dirname + '/delayed-slow-requests.html', 'utf-8')
+  response.send(html)
+})
+
+app.get('/fast', (request, response) => {
   response.json({ foo: 'bar' })
+})
+
+app.get('/slow', (request, response) => {
+  setTimeout(() => {
+    response.json({ foo: 'bar' })
+  }, 1000)
 })
 
 app.listen(8080)
