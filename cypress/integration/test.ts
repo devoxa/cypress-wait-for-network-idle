5f99e17e72e484a8df9ad6e742495c4916ba24e1
queicherius <queicherius@gmail.com>
2021-12-23 14:30:16 +0000

    Add test for longer idle

--- cypress/integration/test.ts
+++ cypress/integration/test.ts
@@ -1,7 +1,13 @@
 describe('cypress-wait-for-network-idle', () => {
-  it('waits for the network to idle before continuing', () => {
+  it('waits for the network to idle before continuing (short idle)', () => {
     cy.visit('http://localhost:8080')
-    cy.waitForNetworkIdle()
-    cy.get('h1', { timeout: 0 }).should('contain', 'Done')
+    cy.waitForNetworkIdle({ timeout: 2000, minIdleTime: 200 })
+    cy.get('h1', { timeout: 0 }).should('contain', 'First')
+  })
+
+  it('waits for the network to idle before continuing (long idle)', () => {
+    cy.visit('http://localhost:8080')
+    cy.waitForNetworkIdle({ timeout: 5000, minIdleTime: 500 })
+    cy.get('h1', { timeout: 0 }).should('contain', 'Second')
   })
 })
